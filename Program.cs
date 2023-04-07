cc7665d788a632cd63bb65520674750be57174f5
Mat_Tol <matvey.talalaevskiy@gmail.com>
2023-04-07 15:37:01 +0300

    Char count

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+static void Main(string args)
+{
+    string text = File.ReadAllText(args);
+    Dictionary<char, int> charFrequencies = new Dictionary<char, int>();
+
+    foreach (char c in text)
+    {
+        if (charFrequencies.ContainsKey(c))
+        {
+            charFrequencies[c]++;
+        }
+        else
+        {
+            charFrequencies[c] = 1;
+        }
+    }
+
+    foreach (KeyValuePair<char, int> kvp in charFrequencies)
+    {
+        Console.WriteLine("{0}: {1}", kvp.Key, kvp.Value);
+    }
+}
+
+
