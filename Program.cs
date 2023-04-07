d5aefb58b183c4bcd287d386c99f4bf00eb3ead3
Mat_Tol <matvey.talalaevskiy@gmail.com>
2023-04-07 18:12:55 +0300

    HuffmanTree

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
-﻿
-static void Main(string args)
+﻿static void Main()
 {
-    string text = File.ReadAllText(args);
+    Dictionary<char, int> charDict = CountFrequency();
+    Node root = BuildHuffmanTree(charDict);
+}
+
+static Dictionary<char, int> CountFrequency()
+{
+    string text = File.ReadAllText("text.txt");
     Dictionary<char, int> charFrequencies = new Dictionary<char, int>();
 
     foreach (char c in text)
@@ -16,10 +21,41 @@ static void Main(string args)
         }
     }
 
-    foreach (KeyValuePair<char, int> kvp in charFrequencies)
-    {
-        Console.WriteLine("{0}: {1}", kvp.Key, kvp.Value);
-    }
+    return charFrequencies;
 }
 
+static Node BuildHuffmanTree(Dictionary<char, int> charFrequencies)
+{
+    var queue = new PriorityQueue<Node, int>();
+
+    foreach (KeyValuePair<char, int> kvp in charFrequencies)
+    {
+        queue.Enqueue(new Node { Symbol = kvp.Key, Frequency = kvp.Value }, kvp.Value);
+    }
+
+    while (queue.Count > 1)
+    {
+        Node left = queue.Dequeue();
+        Node right = queue.Dequeue();
+
+        Node parent = new Node { Frequency = left.Frequency + right.Frequency };
+        parent.LeftChild = left;
+        parent.RightChild = right;
+
+        queue.Enqueue(parent, parent.Frequency);
+    }
+
+    return queue.Dequeue();
+}
+
+
+Main();
+
+class Node
+{
+    public char? Symbol { get; set; }
+    public int Frequency { get; set; }
+    public Node LeftChild { get; set; }
+    public Node RightChild { get; set; }
+}
 
